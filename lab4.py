28f5ca5aed15170718cae38b93421e20a3097f87
Makhmad Karina <kmahmad00@mail.ru>
2023-10-27 18:20:10 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 lab4 = Blueprint('lab4', __name__)
 
 
@@ -17,14 +17,15 @@ def login():
     if username == 'alex' and password == '123':
         return render_template('success1.html', username=username)
     
+    
     error1 = ''
     if username == '':
         error1 = 'Введите логин!'
 
-    if 'username' is session:
+    if 'username' == session:
         render_template(username=username)
         
-    if 'password' is session:
+    if 'password' == session:
         render_template(password=password)
 
     error2 = ''
@@ -33,9 +34,29 @@ def login():
     
    
     error = 'Неверные логин и/или пароль!'
-    
     return render_template('login.html', error=error, error1=error1, error2=error2, username=username, password=password)
 
+
+@lab4.route('/lab4/ice', methods=['GET', 'POST'])
+def ice():
+    if request.method == 'GET':
+        return render_template("ice.html")
+    
+    temperature = request.form.get('temperature')
+    if  not temperature:
+        result = 'ошибка: не задана температура!'
+    elif int(temperature) < -12:
+        result = 'не удалось установить температуру — слишком низкое значение'
+    elif int(temperature) > -1:
+        result = 'не удалось установить температуру — слишком высокое значение'
+    elif int(temperature) > -12 and int(temperature) < -9:
+        result = f"Установлена температура: {temperature}°C ***"
+    elif int(temperature) >= -8 and int(temperature) <= -5:
+        result = f"Установлена температура: {temperature}°C **"
+    elif int(temperature) >= -4 and int(temperature) <= -1:
+        result = f"Установлена температура: {temperature}°C *"
+
+    return render_template("ice.html", result=result)
 
 
     
