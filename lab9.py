10d531428695b70fd41af963c4c38d04052c4501
Makhmad Karina <kmahmad00@mail.ru>
2023-12-21 14:30:18 +0700

    Правки

--- lab9.py
+++ lab9.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, redirect, url_for, render_template, request, session, abort
-from datetime import datetime
+from flask import Blueprint, render_template, request, abort, jsonify, redirect, url_for
+lab9 = Blueprint('laba9', __name__)
 
-lab9 = Blueprint('lab9', __name__)
 
 @lab9.route('/lab9/', methods=['GET', 'POST'])
 def main():
@@ -11,7 +10,7 @@ def main():
         recipient_gender = request.form.get('recipient_gender')
         recipient_name = request.form.get('recipient_name')
 
-        return redirect(url_for('lab9.card', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name))
+        return redirect(url_for('laba9.card', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name))
     else:
         return render_template('lab9/index.html')
 
@@ -25,22 +24,16 @@ def card():
     return render_template('lab9/card.html', sender_name=sender_name, recipient_gender=recipient_gender, recipient_name=recipient_name)
 
 
-
-
-@lab9.route('/lab9/')
-def lab():
-    return render_template('lab9/index.html')
-
 @lab9.app_errorhandler(404)
 def not_found(e):
-    return 'Нет такой страницы', 404
+    return render_template('lab9/error.html'), 404
+
+
+@lab9.route("/lab9/er")
+def index():
+    raise Exception("Произошла ошибка!")
+
 
 @lab9.app_errorhandler(500)
-def internal_error(e):
-    return 'Ошибка', 500
-
-@lab9.route('/lab9/500')
-def error_500():
-    abort(500)
-
-
+def not_found(e):
+    return render_template('lab9/error500.html'), 500
