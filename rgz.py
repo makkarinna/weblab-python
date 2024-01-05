e1da638d558cd47fc474f87369f944ecac4d0d13
Makhmad Karina <kmahmad00@mail.ru>
2024-01-05 18:28:17 +0700

    Правки

--- rgz.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, url_for, render_template, Blueprint, request, session
-from db import db
-
-from db.models import users, articles
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user, logout_user
-
-rgz = Blueprint('laba6', __name__)
